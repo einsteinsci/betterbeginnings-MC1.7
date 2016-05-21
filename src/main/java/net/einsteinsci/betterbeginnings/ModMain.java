66c0b7a4a288ae3f48c12f54a3a94616247e8c54
sealedinterface <sealedinterface@gmail.com>
2016-05-20 20:48:41 -0700

    Version info to 0.9.5-R7

--- src/main/java/net/einsteinsci/betterbeginnings/ModMain.java
+++ src/main/java/net/einsteinsci/betterbeginnings/ModMain.java
@@ -31,8 +31,8 @@ import org.apache.logging.log4j.Level;
 public class ModMain
 {
 	public static final String MODID = "betterbeginnings";
-	public static final String VERSION = "0.9.5-R6";
-	public static final String NAME = "Better Beginnings";
+	public static final String VERSION = "0.9.5-R7";
+	public static final String NAME = "BetterBeginnings";
 	public static final CreativeTabs tabBetterBeginnings = new CreativeTabs("tabBetterBeginnings")
 	{
 		@Override
